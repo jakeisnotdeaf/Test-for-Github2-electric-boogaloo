2cda66f0757074e099691c258824275ad2c24b40
Jacob Levin <yacob.levin3@gmail.com>
2020-01-16 18:13:43 -0500

    why do i even go for it?

---
+++ Test for Github2 electric boogaloo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test_for_Github2_electric_boogaloo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("What a twist");
+
+        }
+    }
+}
